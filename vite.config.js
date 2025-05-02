98d259b10e59df90130736e57ec155feaedfc62c
patehet22062004 <patelhet2264@gmail.com>
2025-05-02 11:49:48 +0530

    editviteconfig.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['het-clothingstore.onrender.com']
+  },
   server: {
     host: '0.0.0.0', // Allow external access
     port: process.env.PORT || 5173,
